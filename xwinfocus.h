78d9e295f17844f6804282239b78dd55f45d072c
Oleksii Milevskyi <123121237+omilevskyi@users.noreply.github.com>
2025-08-28 00:07:23 +0300

    fix: Improve technically without changing logic (#3)

---
+++ xwinfocus.h
@@ -0,0 +1,16 @@
+#include <X11/Xlib.h>
+
+__attribute__((noreturn, format(printf, 2, 3))) static void
+die(int, const char *, ...);
+
+__attribute__((format(printf, 1, 2))) static void warn(const char *, ...);
+
+static void nsleep_ms(int);
+
+static const char *fringe(const char *, char *, char *);
+static Window find_window(Display *, Window, const char *, const char *);
+static Window get_active_window(Display *, Window);
+static void activate_window(Display *, Window, Window);
+static void store_previous_window(Display *, Window, Window);
+static Window retrieve_previous_window(Display *, Window);
+static int print_usage(int, char *);
